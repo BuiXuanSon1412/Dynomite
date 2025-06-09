3bb82b983fb87c81858017227286e53f113814ed
BuiXuanSon1412 <bxson1412@gmail.com>
2025-06-10 05:22:08 +0700

    changes: finish egg batch, shooting egg, next shooting egg position

--- TouchGFX/gui/include/gui/screen1_screen/Screen1View.hpp
+++ TouchGFX/gui/include/gui/screen1_screen/Screen1View.hpp
@@ -4,8 +4,12 @@
 #include <gui_generated/screen1_screen/Screen1ViewBase.hpp>
 #include <gui/screen1_screen/Screen1Presenter.hpp>
 
-#define NUM_ROWS 9
+#include <images/BitmapDatabase.hpp>
+
+#define NUM_ROWS 10
 #define NUM_COLS 9
+#define EGG_WIDTH 26
+#define EGG_HEIGHT 29
 
 class Screen1View : public Screen1ViewBase
 {
@@ -14,12 +18,52 @@ public:
     virtual ~Screen1View() {}
     virtual void setupScreen();
     virtual void tearDownScreen();
+    void handleTickEvent();
     //void onPlayButtonClicked();
 
 protected:
-    touchgfx::Image eggs[NUM_ROWS][NUM_COLS];
+    // varibles to control game flow
+    const int framePerSecond = 30;
+    const int step = 1;
+    int framePerEggBatchUpdate;			// smaller -> harder
+    int frameCountForEggBatchUpdate;
+    uint32_t lastUpdateTickCount;
+
+    // variables to manage the batch of eggs
+    touchgfx::Image eggBatch[NUM_ROWS][NUM_COLS];
+    bool eggBatchState[NUM_ROWS][NUM_COLS];
+    int eggBatchY[NUM_ROWS];
+    int startRowIndex;
+
+    // variable to manage the shooting egg and the next shooting egg
+    touchgfx::Image shootingEgg, nextShootingEgg;
+
+    // array of eggs
+    uint32_t seed;
+    const touchgfx::Bitmap eggBitmaps[6] = {
+    		touchgfx::Bitmap(BITMAP_BLUE_EGG_ID),
+			touchgfx::Bitmap(BITMAP_GREEN_EGG_ID),
+			touchgfx::Bitmap(BITMAP_GREENRED_EGG_ID),
+			touchgfx::Bitmap(BITMAP_GREY_EGG_ID),
+			touchgfx::Bitmap(BITMAP_ORANGE_EGG_ID),
+			touchgfx::Bitmap(BITMAP_WHITE_EGG_ID),
+    };
+    int eggBitmapIndexRange;
 private:
-    void renderEggImages();
+    void initializeEggBatch();
+    void updateEggBatch();
+    void renderEggBatch();
+
+    void initializeShootingEgg();
+    void updateShootingEgg();
+    void renderShootingEgg();
+
+    void initializeNextShootingEgg();
+    void updateNextShootingEgg();
+    void renderNextShootingEgg();
+
+    touchgfx::Bitmap generateRandomEggBitmap();
+    uint32_t lcd_rand();
 };
 
 #endif // SCREEN1VIEW_HPP
--- TouchGFX/gui/src/screen1_screen/Screen1View.cpp
+++ TouchGFX/gui/src/screen1_screen/Screen1View.cpp
@@ -1,17 +1,28 @@
 #include <gui/screen1_screen/Screen1View.hpp>
 
-#include <images/BitmapDatabase.hpp>
-
+#include <cmsis_os.h>
 
 Screen1View::Screen1View()
 {
-
+	startRowIndex = 4;
+	framePerEggBatchUpdate = 10;
+	frameCountForEggBatchUpdate = 0;
+	eggBitmapIndexRange = 3;
+	seed = 123456789;
 }
 
 void Screen1View::setupScreen()
 {
     Screen1ViewBase::setupScreen();
-    renderEggImages();
+    initializeEggBatch();
+    //renderEggBatch();
+
+    initializeShootingEgg();
+    initializeNextShootingEgg();
+
+    playButton.setVisible(false);
+
+    lastUpdateTickCount = osKernelGetTickCount();
 }
 
 void Screen1View::tearDownScreen()
@@ -19,12 +30,143 @@ void Screen1View::tearDownScreen()
     Screen1ViewBase::tearDownScreen();
 }
 
-void Screen1View::renderEggImages() {
+void Screen1View::handleTickEvent() {
+	Screen1ViewBase::handleTickEvent();
+	uint32_t tickPerFrame = osKernelGetTickFreq() / framePerSecond;
+
+	if (osKernelGetTickCount() - lastUpdateTickCount > tickPerFrame) {
+		if (eggBatchY[startRowIndex] >= 200) {
+			playButton.setVisible(true);
+			return;
+		}
+
+		if (frameCountForEggBatchUpdate++ == framePerEggBatchUpdate) {
+			updateEggBatch();
+			renderEggBatch();
+			frameCountForEggBatchUpdate = 0;
+		}
+		updateShootingEgg();
+		renderShootingEgg();
+		lastUpdateTickCount = osKernelGetTickCount();
+	}
+}
+
+void Screen1View::initializeEggBatch() {
+	int i = startRowIndex;
+	int lastY = -1;
+	do {
+		// Y coordinate
+		if (lastY == -1) eggBatchY[i] = startRowIndex * EGG_HEIGHT + 3;
+		else eggBatchY[i] = lastY - EGG_HEIGHT;
+		lastY = eggBatchY[i];
+
+		// set XY
+		int numOfCols = NUM_COLS;
+		int leftPadding = 3;
+		if (i % 2 == 0) {
+			leftPadding = 16;
+			numOfCols--;
+		}
+		for (int j = 0; j < numOfCols; j++) {
+			eggBatch[i][j].setBitmap(generateRandomEggBitmap());
+			eggBatch[i][j].setXY(leftPadding + EGG_WIDTH * j, eggBatchY[i]);
+			eggBatchState[i][j] = true;
+			add(eggBatch[i][j]);
+		}
+		i = i-1;
+		if (i < 0) i += NUM_ROWS;
+	} while (i != startRowIndex);
+}
+
+void Screen1View::renderEggBatch() {
 	for (int i = 0; i < NUM_ROWS; i++) {
-		for (int j = 0; j < NUM_COLS; j++) {
-			eggs[i][j].setBitmap(touchgfx::Bitmap(BITMAP_BLUE_EGG_ID));
-			eggs[i][j].setXY(3+26*i, 3+29*j);
-			add(eggs[i][j]);
+		if (eggBatchY[i] + 0.5 * EGG_HEIGHT <= 0) break;
+		int numOfCols = (i%2 == 0 ? NUM_COLS-1 : NUM_COLS);
+		for (int j = 0; j < numOfCols; j++) {
+			if (eggBatchState[i][j]) {
+				eggBatch[i][j].invalidate();
+			}
 		}
 	}
+}
+
+void Screen1View::updateEggBatch() {
+	// update Y coordinate of all rows in 'eggBatch'
+	for (int i = 0; i < NUM_ROWS; i++) {
+		eggBatchY[i]++;
+		int numOfCols = (i%2 == 0 ? NUM_COLS-1 : NUM_COLS);
+		for (int j = 0; j < numOfCols; j++) {
+			eggBatch[i][j].moveTo(eggBatch[i][j].getX(), eggBatchY[i]);
+		}
+	}
+
+	//if (checkIfShootingEggIsCollided()) { // this validator also update 'shootingEgg' into 'eggBatch'
+		// detect which eggs shooting egg can drop and update 'eggBatchState'
+	//}
+
+
+	int i = startRowIndex;
+	do {
+		// number of eggs in a specific row
+		int numOfCols = (i%2 == 0 ? NUM_COLS-1 : NUM_COLS);
+
+		// update 'startRowIndex'
+		if (eggBatchY[i] + 0.5 * EGG_HEIGHT > 0) {
+			bool empty = true;
+			for (int j = 0; j < numOfCols; j++) {
+				if (eggBatchState[i][j]) empty = false;
+			}
+			if (empty) {
+				startRowIndex--;
+				if (startRowIndex < 0) startRowIndex += NUM_ROWS;
+			}
+		}
+		// prepare eggs which do not show up on screen
+		else {
+			for (int j = 0; j < numOfCols; j++) {
+				if (eggBatchState[i][j] == false) {
+					eggBatch[i][j].setBitmap(generateRandomEggBitmap());
+					eggBatchState[i][j] = true;
+				}
+			}
+		}
+		i = i-1;
+		if (i < 0) i += NUM_ROWS;
+	} while (i != startRowIndex);
+}
+
+
+void Screen1View::initializeShootingEgg() {
+	shootingEgg.setBitmap(generateRandomEggBitmap());
+	shootingEgg.setXY(107, 280);
+	add(shootingEgg);
+}
+void Screen1View::updateShootingEgg() {
+	//sampleEgg.moveTo(sampleEgg.getX(), sampleEgg.getY() + 2);
+}
+
+void Screen1View::renderShootingEgg() {
+	shootingEgg.invalidate();
+}
+
+void Screen1View::initializeNextShootingEgg() {
+	nextShootingEgg.setBitmap(generateRandomEggBitmap());
+	nextShootingEgg.setXY(10, 280);
+	add(nextShootingEgg);
+}
+void Screen1View::updateNextShootingEgg() {
+	//sampleEgg.moveTo(sampleEgg.getX(), sampleEgg.getY() + 2);
+}
+
+void Screen1View::renderNextShootingEgg() {
+	nextShootingEgg.invalidate();
+}
+
+uint32_t Screen1View::lcd_rand() {
+	seed = (1103515245 * seed + 12345) & 0x7fffffff;
+	return seed;
+}
+touchgfx::Bitmap Screen1View::generateRandomEggBitmap() {
+	uint32_t randomEggBitmapIndex = lcd_rand() % eggBitmapIndexRange;
+	return eggBitmaps[randomEggBitmapIndex];
 }
